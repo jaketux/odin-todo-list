05b92e550b2180fc7a8106ba6c3c5d5645327db6
jaketux <169253660+jaketux@users.noreply.github.com>
2024-11-25 08:41:55 +1100

    To Do Working on the To Do Project, lots to fix including adding projects, adding To Do's to existing projects, etc.
    I have prepared a list on paper and I am working through this one by one to help with workflow.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import "./styles.css";
 import { format } from "date-fns";
 
 
-const project = projectModule();
-
 function projectModule(){
     
     let listOfProjects = [
@@ -20,51 +18,48 @@ function projectModule(){
         }
     ]
 
+    function getProjects(){
+        return listOfProjects
+    }
+
     function createProject(name){
-    
         let project = {
             name: name,
             toDos: []
         }
-
-        for (var i = 0; i<=listOfProjects.length; i++){
-            if (listOfProjects[i].name !== name){
-                listOfProjects.push(project)
-                console.log(listOfProjects)
-            }
-        }
-    }
-
-    function pushProjecToList(projectName){
-
-    }
-    
+        listOfProjects.push(project)
+    } 
 
     function addToDoToProject(projectname){
         listOfProjects.projectname.toDos.push()
         console.log(listOfProjects)
     }
 
-    return {listOfProjects, createProject, addToDoToProject, pushProjecToList}
-
+        return {listOfProjects, createProject, addToDoToProject,getProjects}
 }
 
-    function createToDo (name, description, priority, status, dueDate){
+    function createToDo (name, description, priority, status, dueDate, project){
+
+        const project = projectModule();
+
         let toDo = {
             name: name,
             description: description,
             priority: priority,
             status: status,
             dueDate: dueDate,
-            assignedProject: 0,
         }
-        return toDo
 
+        for (var i = 0; i<=projectArray.length;i++)
+            if (projectArray[i].name === project){
+                projectArray[i].push(toDo)
+            }
     }
     
     
-console.log(createToDo("Rob your bank", "With a gun", "High", "Incomplete", "24/11/2024", "Rob Bank"))
-
-project.createProject("Study The Odin Project")
+  
+    createToDo("Rob your bank", "With a gun", "High", "Incomplete", "24/11/2024", "Rob Bank")
+    
+    
 
 
